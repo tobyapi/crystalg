6ebfbaac511522f698a72e74891d48b3e4668889
tobias <landofscala@gmail.com>
2017-03-10 12:55:13 +0900

    Add requiring

--- src/crystalg.cr
+++ src/crystalg.cr
@@ -1,4 +1,8 @@
 require "./crystalg/*"
+require "./crystalg/graph/*"
+require "./crystalg/data_structures/*"
+require "./crystalg/graph/searching/*"
+require "./crystalg/strings/*"
 
 module Crystalg
   # TODO Put your code here
