34f0acffdea6db01883fc1da6a650436f6f29810
tobias <s1190170@gmail.com>
2017-03-13 23:17:56 +0900

    add ccw

--- src/crystalg/geometry/geometry.cr
+++ src/crystalg/geometry/geometry.cr
@@ -2,8 +2,22 @@ require "./*"
 
 module Crystalg::Geometry
 
+  # a -> b -> c
   enum CCW
-
+    COUNTER_CLOCKWISE = 1
+    CLOCKWISE = -1
+    ONLINE_BACK = 2   # c--a--b
+    ONLINE_FRONT = -2 # a--b--c
+    ON_SEGMENT = 0    # a--c--b
   end
-
+  
+  def counter_clockwise(a : Point, b : Point, c : Point) : CCW
+    b = b - a
+    c = c - a
+    return COUNTER_CLOCKWISE if b.cross c > 0
+    return CLOCKWISE         if b.corss c < 0
+    return ONLINE_BACK       if b.dot c < 0
+    return ONLINE_FRONT      if b.norm < c.norm
+    ON_SEGMENT
+  end
 end
--- src/crystalg/geometry/point.cr
+++ src/crystalg/geometry/point.cr
@@ -21,5 +21,35 @@ module Clystalg::Geometry
     def /(other : Float)
       Point.new(x / other, y / other)
     end
+    
+    def norm : Float
+      (x * x + y * y).sqrt
+    end
+    
+    def dist(other : Point) : Float
+      (self - other).norm
+    end
+    
+    def dot(other : Point) : Float
+      x * other.x + y * other.y
+    end
+    
+    def cross(other : Point) : Float
+      x * other.y - y * other.x
+    end
+    
+    def rotate(radian : Float, pivot : Point = Point.new(0,0))
+      Point.new(
+        LibM.cos(r) * (x - pivot.x) - LibM.sin(r) * (y - pivot.y) + pivot.x,
+        LibM.sin(r) * (x - pivot.x) + LibM.cos(r) * (y - pivot.y) + pivot.y)
+    end
+    
+    def arg : Float
+      LibM.atan(y/x) if x.sign > 0
+      LibM.atan(y/x) + PI if x.sign < 0
+      PI / 2.0.as Float  if y.sign > 0
+      3.0 * PI / 2.0.as Float if y.sign < 0
+      0
+    end
   end
 end
