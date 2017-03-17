9dfb776d54977623aceb25d3543d8b8e05e03f51
tobias <landofscala@gmail.com>
2017-03-17 18:29:41 +0900

    Add segment and circle

--- src/crystalg/geometry/point.cr
+++ src/crystalg/geometry/point.cr
@@ -21,29 +21,29 @@ module Crystalg::Geometry
     def /(other : Float)
       Point.new(x / other, y / other)
     end
-    
+
     def norm : Float
       (x * x + y * y).as(Float).sqrt
     end
-    
-    def dist(other : Point) : Float
+
+    def distance(other : Point) : Float
       (self - other).norm
     end
-    
+
     def dot(other : Point) : Float
       x * other.x + y * other.y
     end
-    
+
     def cross(other : Point) : Float
       x * other.y - y * other.x
     end
-    
+
     def rotate(radian : Float, pivot : Point = Point.new(0,0))
       Point.new(
         LibM.cos(r) * (x - pivot.x) - LibM.sin(r) * (y - pivot.y) + pivot.x,
         LibM.sin(r) * (x - pivot.x) + LibM.cos(r) * (y - pivot.y) + pivot.y)
     end
-    
+
     def arg : Float
       LibM.atan(y/x) if x.sign > 0
       LibM.atan(y/x) + PI if x.sign < 0
