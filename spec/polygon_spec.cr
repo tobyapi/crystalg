b15fee1ddc2815bddf9f31a7103bc1264d531b34
tobias <s1190170@gmail.com>
2017-03-19 19:17:42 +0900

    add testing for convex-hull and convex-cut

--- spec/polygon_spec.cr
+++ spec/polygon_spec.cr
@@ -51,4 +51,41 @@ describe Crystalg do
     true.should eq(pol.contain(Point.new(0.0, 2.0)) === Polygon::Containment::ON)
     true.should eq(pol.contain(Point.new(3.0, 2.0)) === Polygon::Containment::OUT)
   end
+  
+  it "convex hull" do
+    pol = Polygon.new(
+      Point.new(2.0, 1.0),
+      Point.new(0.0, 0.0),
+      Point.new(1.0, 2.0),
+      Point.new(2.0, 2.0),
+      Point.new(4.0, 2.0),
+      Point.new(1.0, 3.0),
+      Point.new(3.0, 3.0)
+    )
+    
+    ans = Polygon.new(
+      Point.new(0.0, 0.0),
+      Point.new(2.0, 1.0),
+      Point.new(4.0, 2.0),
+      Point.new(3.0, 3.0),
+      Point.new(1.0, 3.0)
+    )
+    
+    true.should eq(pol.convex_hull === ans)
+  end
+  
+  it "convex cut" do
+    pol = Polygon.new(
+      Point.new(1.0, 1.0),
+      Point.new(4.0, 1.0),
+      Point.new(4.0, 3.0),
+      Point.new(1.0, 3.0)
+    )
+    
+    res = pol.convex_cut(Line.new(Point.new(2.0, 0.0), Point.new(2.0, 4.0)))
+    true.should eq(res.area === 2.0)
+    
+    res = pol.convex_cut(Line.new(Point.new(2.0, 4.0), Point.new(2.0, 0.0)))
+    true.should eq(res.area === 4.0)
+  end
 end
