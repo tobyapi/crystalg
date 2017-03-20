43e8f56d4d2c00a28cd436f4c6bae5dce34612dc
tobias <s1190170@gmail.com>
2017-03-20 14:22:22 +0900

    Add Leftist Heap

---
+++ spec/leftist_heap_spec.cr
@@ -0,0 +1,26 @@
+require "./spec_helper"
+
+include Crystalg::Graph
+
+describe Crystalg do
+  it "LeftistHeap" do
+    heap = LeftistHeap(Int32).new
+    
+    heap.push 5
+    heap.push 3
+    heap.push 8
+    true.should eq(heap.top === 3)
+    
+    heap.push 1
+    true.should eq(heap.top === 1)
+    
+    heap.pop
+    true.should eq(heap.top === 3)
+    
+    heap.pop
+    true.should eq(heap.top === 5)
+    
+    heap.pop
+    true.should eq(heap.top === 8)
+  end
+end
