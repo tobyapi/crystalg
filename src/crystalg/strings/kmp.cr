62d7e41adc5fb55c7e10dfe1d069c23f6a3e6bcb
tobias <s1190170@gmail.com>
2017-03-20 21:09:52 +0900

    Add kmp

---
+++ src/crystalg/strings/kmp.cr
@@ -0,0 +1,29 @@
+
+module Crystalg::Strings
+  def kmp(target : String, pattern : String): Array(Int32)
+    # initialize
+    n = Array(Int32).new(pattern.size+1, 0)
+    j, n[0] = -1, -1
+    (0...pattern.size).each do |i|
+      while j >= 0 && pattern[i] != pattern[j]
+        j = n[j]
+      end
+      j = j + 1
+      n[i+1] = j
+    end
+
+    result = Array(Int32).new
+    j = 0
+    (0...target.size).each do |i|
+      while j >= 0 && target[i] != pattern[j]
+        j = n[j]
+      end
+      j = j.succ
+      if j == pattern.size
+        result << i - pattern.size + 1
+        j = n[j]
+      end
+    end
+    result
+  end
+end
