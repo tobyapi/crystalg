03c9f8e28cd26f60d81ca881d21647d90c15f23a
tobias <s1190170@gmail.com>
2017-03-26 17:19:33 +0900

    Add ford fulkerson

--- src/crystalg/graph/flow/ford_fulkerson.cr
+++ src/crystalg/graph/flow/ford_fulkerson.cr
@@ -1,8 +1,33 @@
 require "../representation/adjacency_list/flow_graph"
 
-module Crystlag::Graph
+module Crystalg::Graph
 
-
-  def max_flow(graph : Graph)
+  # C: type of capacity
+  class FordFulkerson(C)
+    def initialize(@graph : FlowGraph(C))
+      @used = Array(Bool).new(@graph.size, false)
+    end
+    
+    def max_flow(source : NodeID, target : NodeID)
+      flow = C.zero
+      loop do
+        @used.fill(false)
+        f = dfs(source, target, C::MAX)
+        return flow if f == C.zero
+        flow += f
+      end
+    end
+    
+    def dfs(v : NodeID, target : NodeID, flow : C): C
+      return flow if v == target
+      @used[v] = true
+      @graph.adjacent(v).each_with_index do |e, i|
+        if !@used[e.target] && e.capacity > C.zero
+          d = dfs(e.target, target, Math.min(flow, e.capacity))
+          return @graph.flow(v,i,d) if d > C.zero
+        end
+      end
+      C.zero
+    end
   end
 end
