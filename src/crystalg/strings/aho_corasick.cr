c8d24701ca2211ca32b559c44a34af51df9999b9
tobias <s1190170@gmail.com>
2017-03-31 21:32:21 +0900

    Add testing to aho_corasick

--- src/crystalg/strings/aho_corasick.cr
+++ src/crystalg/strings/aho_corasick.cr
@@ -1,7 +1,9 @@
 
 module Crystalg::Strings
   class AhoCorasick
-    struct Node
+    class Node
+      property parent, link, char, is_leaf
+      
       @parent : Int32
       @link : Int32
       @char : Char #from parent
@@ -17,40 +19,47 @@ module Crystalg::Strings
         @children = Array(Int32).new(128, -1)
         @next = Array(Int32).new(128, -1)
       end
+      
+      def set_char(@char : Char)
+      end
+      
+      def get_char
+        @char
+      end
     end
     
     @size : Int32 # number of node
     @nodes : Array(Node)
+    ROOT = 0
     
     def initialize(max_node : Int32)
-      @size = 0 
-      @nodes = Array(Node).new(max_node) { Nodes.new }
-      @root = 0
-      @nodes[@root].@link = @root
-      @nodes[@root].@parent = -1
+      @size = 0
+      @nodes = Array(Node).new(max_node) { Node.new }
+      @nodes[ROOT].link = ROOT
+      @nodes[ROOT].parent = -1
     end
     
     def add(str : String)
-      cur = 0
-      str.each do |ch|
+      cur = ROOT
+      str.each_char do |ch|
         c = ch.bytes.first
-        if @nodes[cur].@child[c] != -1
-          @nodes[@size].@parent = cur
-          @nodes[@size].@char = ch
-          @nodes[cur].@children[c] = @size
+        if @nodes[cur].@children[c] == -1
           @size += 1
+          @nodes[@size].parent = cur
+          @nodes[@size].char = ch
+          @nodes[cur].@children[c] = @size
         end
         cur = @nodes[cur].@children[c]
       end
-      @nodes[cur].@is_leaf = true
+      @nodes[cur].is_leaf = true
     end
     
     def failure(id : Int32) : Int32
       if @nodes[id].link == -1
-        if @nodes[id].parent == 0
-          @nodes[id].link = @root
+        if @nodes[id].parent == ROOT
+          @nodes[id].link = ROOT
         else
-          @nodes[id].link = goto(failure(@nodes[id].parent), @nodes[id].@char)
+          @nodes[id].link = goto(failure(@nodes[id].parent), @nodes[id].char)
         end
       end
       @nodes[id].link
@@ -58,14 +67,33 @@ module Crystalg::Strings
     
     def goto(id : Int32, char : Char) : Int32
       c = char.bytes.first
-      if @nodes[id].@next[c] != -1
-        @nodes[id].@next[c] = @nodes[id].@children[c]
-      elsif id == 0
-        @nodes[id].@next[c] = @root
-      else
-        @nodes[id].@next[c] = goto(failure(id), char)
+      if @nodes[id].@next[c] == -1
+        if @nodes[id].@children[c] != -1
+          @nodes[id].@next[c] = @nodes[id].@children[c]
+        elsif id == ROOT
+          @nodes[id].@next[c] = ROOT
+        else
+          @nodes[id].@next[c] = goto(failure(id), char)
+        end
       end
       @nodes[id].@next[c]
+    end
+    
+    def contain?(target : String)
+      cur = ROOT
+      target.each_char_with_index do |ch, i|
+        cur = @nodes[cur].@children[ch.bytes.first]
+        return false if cur < 0
+      end
+      @nodes[cur].is_leaf
+    end
+    
+    def match?(target : String)
+      cur = ROOT
+      target.each_char_with_index do |ch, i|
+        cur = goto(cur, ch)
+      end
+      @nodes[cur].is_leaf
     end
   end
 end
