96cac81526d8dbabb5c24ecbe503279d3f817978
tobias <s1190170@gmail.com>
2017-06-06 02:32:52 +0900

    Refactoring Fenwick

--- src/crystalg/data_structures/trees/fenwick_tree.cr
+++ src/crystalg/data_structures/trees/fenwick_tree.cr
@@ -1,21 +1,23 @@
 
 module Crystalg::Trees
   class FenwickTree(T)
-    def initialize(@size : Int32)
+    @size : Int32
+    
+    def initialize(@size)
       @data = Array(T).new(@size * 2 - 1, T.zero)
     end
 
-    def add(key : Int32, value : T)
+    def add(key, value)
       while key <= @size
-        @data[key] = @data[key] + value
-        key = key + (key & (-key))
+        @data[key] += value
+        key = key + (key & -key)
       end
     end
 
-    def sum(key : Int32): T
+    def sum(key)
       result = T.zero
       while key > T.zero
-        result = result + @data[key]
+        result += @data[key]
         key = key - (key & -key)
       end
       result
