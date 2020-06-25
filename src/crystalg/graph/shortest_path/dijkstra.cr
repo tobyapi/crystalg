27ebebe69e00e9f2641bc395590cf36982eaef6d
TobiasGSmollett <landofscala@gmail.com>
2020-06-25 12:00:52 +0900

    Rmoves State struct

--- src/crystalg/graph/shortest_path/dijkstra.cr
+++ src/crystalg/graph/shortest_path/dijkstra.cr
@@ -3,39 +3,33 @@ require "../graph"
 module Crystalg::Graph::ShortestPath
 
   module Dijkstra(C)
-    private class State(C)
-      include Comparable(State)
-
-      def initialize(@node_id : NodeID, @cost : C, @from : NodeID)
-      end
-
-      def ==(that : self)
-        @node_id == that.@node_id && @cost == that.@cost && @from == that.@from
-      end
-
-      def <=>(other : State)
-        @cost <=> other.@cost
-      end
-    end
-
     def dijkstra(start : NodeID): Array(Tuple(C, NodeID?)?)
-      que = PriorityQueue(State(C)).new
+      que = PriorityQueue(Tuple(C, NodeID, NodeID)).new
       result = Array(Tuple(C, NodeID?)?).new(@size, nil)
 
-      start_state = State(C).new(start, 0, -1)
+      start_state = {0, start, -1}
       que.push(start_state)
       result[start] = {0, nil}
 
       until (current_state = que.pop!).nil?
-        adjacent(current_state.@node_id).map do |edge|
-          cost = current_state.@cost + edge.cost
-          next if !result[edge.target].nil? && result[edge.target].as(Tuple(C, NodeID?))[0] < cost
-          next_state = State(C).new(edge.target, cost, edge.source)
+        adjacent(current_state[1]).map do |edge|
+          target_result = result[edge.target]
+          is_visited = !target_result.nil?
+          cost_to_target_node = current_state[0] + edge.cost
+
+          next if is_visited && is_lower?(cost_to_target_node, target_result)
+
+          next_state = {cost_to_target_node, edge.target, edge.source}
           que.push(next_state)
-          result[edge.target] = {cost, edge.source}
+          result[edge.target] = {cost_to_target_node, edge.source}
         end
       end
       result
+    end
+
+    private def is_lower?(cost_to_target_node : C, target_result : Tuple(C, NodeID?)?)
+      target_node_result = target_result.as(Tuple(C, NodeID?))
+      target_node_result[0] < cost_to_target_node
     end
 
     def get_dijkstra_path(node_id : NodeID, dijkstra_result : Array(Tuple(C, NodeID?)?)): Array(NodeID)
