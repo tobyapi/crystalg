8d90aa9a1dab667115a6788fcda78e1b90c02115
TobiasGSmollett <landofscala@gmail.com>
2020-07-10 22:36:42 +0900

    Refactoring RBST

--- src/crystalg/data_structures/randomized_binary_search_tree.cr
+++ src/crystalg/data_structures/randomized_binary_search_tree.cr
@@ -1,36 +1,22 @@
 module Crystalg::DataStructures
   class RandomizedBinarySearchTree(T)
-    class Empty
-      @size = 0
-      @rev = false
 
-      getter size, rev
-      setter rev
-
-      private def initialize
-      end
-
-      def self.instance : Empty
-        @@instance ||= new
-      end
-    end
-
-    class TreeNode(T)
+    class Node(T)
       def initialize(
         @value : T,
-        @left : (TreeNode(T) | Empty) = Empty.instance,
-        @right : (TreeNode(T) | Empty) = Empty.instance,
+        @left : Node(T)?  = nil,
+        @right : Node(T)? = nil,
         @rev : Bool = false,
         @size : Int32 = 0
       )
       end
 
-      getter left : (TreeNode(T) | Empty)
-      getter right : (TreeNode(T) | Empty)
+      getter left : Node(T)?
+      getter right : Node(T)?
       getter value, size, rev
 
-      setter left : (TreeNode(T) | Empty)
-      setter right : (TreeNode(T) | Empty)
+      setter left : Node(T)?
+      setter right : Node(T)?
       setter size, rev
     end
 
@@ -38,31 +24,32 @@ module Crystalg::DataStructures
       @rnd = ::Random.new
     end
 
-    @root : (TreeNode(T) | Empty) = Empty.instance
+    @root : Node(T)? = nil
 
-    private def propagate(t : TreeNode(T)) : TreeNode(T)
-      t.size = t.left.size + t.right.size + 1
+    private def propagate(t : Node(T)) : Node(T)
+      left_size = t.left.nil? ? 0 : t.left.as(Node).size
+      right_size = t.right.nil? ? 0 : t.right.as(Node).size
+      t.size = left_size + right_size + 1
 
-      if (t.rev == true)
+      if t.rev
         t.left, t.right = t.right, t.left
-
-        t.left.rev ^= true
-        t.right.rev ^= true
+        t.left.as(Node).rev ^= true if !t.left.nil?
+        t.right.as(Node).rev ^= true if !t.right.nil?
         t.rev = false
       end
       t
     end
 
-    private def merge_rec(l : (TreeNode(T) | Empty), r : (TreeNode(T) | Empty)) : (TreeNode(T) | Empty)
-      l = propagate l if !l.is_a? Empty
-      r = propagate r if !r.is_a? Empty
+    private def merge_rec(l : Node(T)?, r : Node(T)?) : Node(T)?
+      l = propagate l if !l.nil?
+      r = propagate r if !r.nil?
 
-      if (l.is_a? Empty || r.is_a? Empty)
-        return !l.is_a?(Empty) ? l : r
+      if l.nil? || r.nil?
+        return !l.nil? ? l : r
       end
 
       m, n = l.size, r.size
-      if (@rnd.next_int % (m + n) < m)
+      if @rnd.next_int % (m + n) < m
         l.right = merge_rec l.right, r
         propagate l
       else
@@ -71,36 +58,37 @@ module Crystalg::DataStructures
       end
     end
 
-    def merge(l : (TreeNode(T) | Empty), r : (TreeNode(T) | Empty)) : (TreeNode(T) | Empty)
+    def merge(l : Node(T)?, r : Node(T)?) : Node(T)?
       (@root = merge_rec(l, r))
     end
 
-    def split(k : Int32, t : (TreeNode(T) | Empty) = @root)
-      t = propagate t if !t.is_a? Empty
-      return {Empty.instance, Empty.instance} if t.is_a? Empty
-      if (k <= t.left.size)
+    def split(k : Int32, t : Node(T)? = @root)
+      t = propagate t if !t.nil?
+      return {nil, nil} if t.nil?
+      left_size = t.left.nil? ? 0 : t.left.as(Node(T)).size
+      if k <= left_size
         l, r = split k, t.left
         t.left = r
         {l, propagate t}
       else
-        l, r = split(k - t.left.size - 1, t.right)
+        l, r = split(k - left_size - 1, t.right)
         t.right = l
         {propagate(t), r}
       end
     end
 
-    private def insert(k : Int32, value : T, t : (TreeNode(T) | Empty))
+    private def insert(k : Int32, value : T, t : Node(T)?)
       l, r = split k
-      t = merge_rec(l, TreeNode.new value)
+      t = merge_rec(l, Node.new value)
       t = merge_rec t, r
-      t.is_a?(Empty) ? t : propagate(t)
+      t.nil? ? t : propagate(t)
     end
 
     private def erase(k, t)
       tmp, c = split k + 1, t
       a, b = split k, tmp
       t = merge_rec a, c
-      t = propagate(t) if !t.is_a? Empty
+      t = propagate(t) if !t.nil?
       t
     end
 
@@ -113,22 +101,23 @@ module Crystalg::DataStructures
     end
 
     def find(k, t = @root)
-      return nil if t.is_a? Empty
+      return nil if t.nil?
       propagate t
 
-      if (k < t.left.size)
+      left_size = t.left.nil? ? 0 : t.left.as(Node(T)).size
+      if k < left_size
         find k, t.left
-      elsif (t.left.size == k)
+      elsif left_size == k
         t.value
       else
-        find k - t.left.size - 1, t.right
+        find k - left_size - 1, t.right
       end
     end
 
     def reverse(left_id, right_id)
       tmp, c = split right_id
       a, b = split left_id, tmp
-      b.rev ^= true
+      b.as(Node(T)).rev ^= true if !b.nil?
       merge_rec(merge_rec(a, b), c)
     end
   end
